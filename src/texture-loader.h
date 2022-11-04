fcf54d484e57de22a3b4535b3ffaa10c94deb89a
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2022-11-05 01:02:49 +0800

    Set texture filtering to `GL_NEAREST`

--- src/texture-loader.h
+++ src/texture-loader.h
@@ -18,10 +18,10 @@ class TextureLoader
 			int width, height, channels;
 			unsigned int texture;
 			glGenTextures(1, &texture);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,     GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,     GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 			this->textures[filepath] = texture;
 
 			unsigned char *data = stbi_load(filepath, &width, &height, &channels, 4);
