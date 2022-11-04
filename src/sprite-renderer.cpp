ec5698259faefd221ffe640f83047f3cd0a63c4b
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2022-11-04 23:19:47 +0800

    Update `SpriteRenderer` shaders to support textures and colouring

--- src/sprite-renderer.cpp
+++ src/sprite-renderer.cpp
@@ -9,10 +9,15 @@ SpriteRenderer::SpriteRenderer()
 {
 	// Compile Shader
 	this->shader = this->compileShader();
-	this->modelMatrixLocation = glGetUniformLocation(this->shader, "modelMatrix");
-	this->viewMatrixLocation = glGetUniformLocation(this->shader, "viewMatrix");
-	this->projectionMatrixLocation = glGetUniformLocation(this->shader, "projectionMatrix");
+	this->modelMatrixLocation = glGetUniformLocation(this->shader, "v_model_matrix");
+	this->viewMatrixLocation = glGetUniformLocation(this->shader, "v_view_matrix");
+	this->projectionMatrixLocation = glGetUniformLocation(this->shader, "v_projection_matrix");
 	this->texture = 0;
+
+	// Set Default Uniforms
+	glUseProgram(this->shader);
+	glUniform4f(glGetUniformLocation(this->shader, "f_colour"), 1.0f, 1.0f, 1.0f, 1.0f);
+	glUseProgram(0);
 
 	// Generate VAO
 	glGenVertexArrays(1, &(this->vao));
@@ -20,10 +25,10 @@ SpriteRenderer::SpriteRenderer()
 
 	// Generate VBO (data)
 	float vertices[] = {
-		-0.5f, -0.5f,
-		-0.5f,  0.5f,
-		 0.5f, -0.5f,
-		 0.5f,  0.5f,
+		-0.5f, -0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  1.0f, 1.0f,
 	};
 
 	// Generate VBO (state)
@@ -44,9 +49,14 @@ SpriteRenderer::SpriteRenderer()
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// Set Array Attribute `position`
-	GLint positionAttribute = glGetAttribLocation(this->shader, "position");
-	glVertexAttribPointer(positionAttribute, 2, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(positionAttribute);
+	int position = glGetAttribLocation(this->shader, "v_position");
+	glVertexAttribPointer(position, 2, GL_FLOAT, GL_FALSE, 4*sizeof(float), (void*)(0*sizeof(float)));
+	glEnableVertexAttribArray(position);
+
+	// Set Array Attribute `texture_coordinate`
+	int texture_coordinate = glGetAttribLocation(this->shader, "v_texture_coordinate");
+	glVertexAttribPointer(texture_coordinate, 2, GL_FLOAT, GL_FALSE, 4*sizeof(float), (void*)(2*sizeof(float)));
+	glEnableVertexAttribArray(texture_coordinate);
 
 	// Unbind State
 	glBindVertexArray(0);
@@ -57,14 +67,17 @@ SpriteRenderer::SpriteRenderer()
 const char* SpriteRenderer::getVertexShaderSource()
 {
 	return R"glsl(
-		#version 150 core
-		in vec2 position;
-		uniform mat4 modelMatrix;
-		uniform mat4 viewMatrix;
-		uniform mat4 projectionMatrix;
+		#version 330 core
+		in vec2 v_position;
+		in vec2 v_texture_coordinate;
+		out vec2 f_texture_coordinate;
+		uniform mat4 v_model_matrix;
+		uniform mat4 v_view_matrix;
+		uniform mat4 v_projection_matrix;
 		void main()
 		{
-			gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 0.0, 1.0);
+			f_texture_coordinate = v_texture_coordinate;
+			gl_Position = v_projection_matrix * v_view_matrix * v_model_matrix * vec4(v_position, 0.0, 1.0);
 		}
 	)glsl";
 }
@@ -72,13 +85,14 @@ const char* SpriteRenderer::getVertexShaderSource()
 const char* SpriteRenderer::getFragmentShaderSource()
 {
 	return R"glsl(
-		#version 150 core
-
-		out vec4 fragColour;
-
+		#version 330 core
+		in vec2 f_texture_coordinate;
+		out vec4 colour;
+		uniform sampler2D f_texture;
+		uniform vec4 f_colour;
 		void main()
 		{
-			fragColour = vec4(1.0, 0.0, 0.0, 1.0);
+			colour = texture(f_texture, f_texture_coordinate) * f_colour;
 		}
 	)glsl";
 }
