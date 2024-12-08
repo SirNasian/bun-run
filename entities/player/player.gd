f1d7f83c4b52427bf61530249f101407211458bc
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2024-12-08 13:28:34 +0800

    Implement coyote time

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+const COYOTE_TIME = 0.05
 const MAX_SPEED = 256
 const GRAVITY = 1024
 
@@ -9,8 +10,11 @@ var input = {
 	"jump": 0,
 }
 
+var can_jump = self.COYOTE_TIME
+
 func _process(delta: float) -> void:
 	var input_x = float(input.left + input.right)
+	self.update_coyote_time(delta)
 	self.update_velocity(input_x, delta)
 	self.update_animation()
 	self.move_and_slide()
@@ -21,17 +25,22 @@ func _input(event: InputEvent) -> void:
 	if (event.is_action_released("player_left")):  input.left  = 0
 	if (event.is_action_pressed("player_right")):  input.right = event.get_action_strength("player_right")
 	if (event.is_action_released("player_right")): input.right = 0
-	if (event.is_action_pressed("player_jump")):   input.jump  = 1 if (self.is_on_floor()) else 0
+	if (event.is_action_pressed("player_jump")):   input.jump  = 1
+
+func update_coyote_time(delta: float) -> void:
+	if (self.is_on_floor()):
+		self.can_jump = self.COYOTE_TIME
+	else:
+		self.can_jump = max(0, self.can_jump - delta)
 
 func update_velocity(input_x: float, delta: float) -> void:
-	if (self.is_on_floor()):
-		if (self.input.jump):
-			self.input.jump = 0
-			self.velocity.y = -256
-		self.velocity.x = lerp(self.velocity.x, input_x * self.MAX_SPEED, delta * 4)
-	else:
+	self.velocity.x = lerp(self.velocity.x, input_x * self.MAX_SPEED, delta * (1 + (int(self.is_on_floor()) * 3)))
+	if (self.input.jump && self.can_jump):
+		self.input.jump = 0
+		self.can_jump = 0
+		self.velocity.y = -256
+	if (!self.is_on_floor()):
 		self.velocity.y += self.GRAVITY * delta
-		self.velocity.x = lerp(self.velocity.x, input_x * self.MAX_SPEED, delta)
 
 func update_animation() -> void:
 	var sprite = $AnimatedSprite2D as AnimatedSprite2D
