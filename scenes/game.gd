b91a70db92ecc449589dbfe439c376046f9b1274
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2024-12-10 16:44:51 +0800

    Refactor `game.gd` for readability

--- scenes/game.gd
+++ scenes/game.gd
@@ -17,68 +17,28 @@ func _ready() -> void:
 
 	$PlayerSpawner.spawn_function = Player.instantiate
 
-	self.multiplayer.connection_failed.connect(_quit)
-	self.multiplayer.server_disconnected.connect(_quit)
+	self.multiplayer.connection_failed.connect(_connection_failed)
+	self.multiplayer.connected_to_server.connect(_server_connected)
+	self.multiplayer.server_disconnected.connect(_server_disconnected)
 	self.multiplayer.peer_connected.connect(_peer_connected)
 	self.multiplayer.peer_disconnected.connect(_peer_disconnected)
 
 	if (config.is_server()):
-		self._host_server(config.get_server_bind_address(), config.get_server_port())
+		self.host_server(config.get_server_bind_address(), config.get_server_port())
 	else:
-		self._connect_server(config.get_client_server_address(), config.get_client_port())
+		self.connect_server(config.get_client_server_address(), config.get_client_port())
 
 
-func _exit_tree() -> void:
-	config.free()
+func _connection_failed() -> void:
+	print("failed to connect to server")
 
 
-func _host_server(address: String, port: int) -> void:
-	var tls = _get_server_tls("res://private.key", "res://certificate.crt")
-	if (!tls): tls = _get_server_tls(config.get_server_certificate_key_path(), config.get_server_certificate_path())
-	address = address if address else config.get_server_bind_address()
-	port = port if port else config.get_server_port()
-
-	var peer: WebSocketMultiplayerPeer = WebSocketMultiplayerPeer.new()
-	peer.create_server(port, address, tls)
-	self.multiplayer.multiplayer_peer = peer
-
-	if (!OS.has_feature("dedicated_server")):
-		$PlayerSpawner.spawn(self.get_multiplayer_authority())
-
-	print("hosting: %s" % { "address": address, "port": port, "tls": !!tls })
+func _server_connected() -> void:
+	print("connected to server")
 
 
-func _get_server_tls(key_path: String, cert_path: String) -> TLSOptions:
-	var exists = ResourceLoader.exists(key_path) && ResourceLoader.exists(cert_path)
-	return TLSOptions.server(load(key_path), load(cert_path)) if (exists) else null
-
-
-func _connect_server(address: String, port: int) -> void:
-	var tls = _get_client_tls("res://certificate.crt")
-	if (!tls): tls = _get_client_tls(config.get_server_certificate_path())
-	var protocol = "wss" if (tls) else "ws"
-	address = address if address else config.get_client_server_address()
-	port = port if port else config.get_client_port()
-
-	var peer: WebSocketMultiplayerPeer = WebSocketMultiplayerPeer.new()
-	peer.create_client("%s://%s:%d" % [protocol, address, port], tls)
-	self.multiplayer.multiplayer_peer = peer
-
-	print("connecting: %s" % { "address": address, "port": port, "tls": !!tls })
-
-
-func _get_client_tls(cert_path: String) -> TLSOptions:
-	var exists = ResourceLoader.exists(cert_path)
-	return TLSOptions.client(load(cert_path)) if (exists) else null
-
-
-func hide_connection_menu() -> void:
-	var connection_menu: ConnectionMenu = $CanvasLayer/CenterContainer/ConnectionMenu
-	connection_menu.visible = false
-
-
-func _quit() -> void:
-	self.get_tree().quit()
+func _server_disconnected() -> void:
+	print("disconnected from server")
 
 
 func _peer_connected(id: int) -> void:
@@ -94,3 +54,52 @@ func _peer_disconnected(id: int) -> void:
 			if (player.get_multiplayer_authority() == id):
 				player.queue_free()
 				break
+
+func host_server(address: String, port: int) -> void:
+	var get_tls = func (paths: Array) -> TLSOptions:
+		for p in paths:
+			if (ResourceLoader.exists(p[0]) && ResourceLoader.exists(p[1])):
+				var tls = TLSOptions.server(load(p[0]), load(p[1]))
+				if (tls): return tls
+		return null
+
+	var tls_options = get_tls.call([
+		[config.get_server_certificate_key_path(), config.get_server_certificate_path()],
+		["res://private.key", "res://certificate.crt"],
+	])
+
+	address = address if address else config.get_server_bind_address()
+	port = port if port else config.get_server_port()
+
+	var peer: WebSocketMultiplayerPeer = WebSocketMultiplayerPeer.new()
+	peer.create_server(port, address, tls_options)
+	self.multiplayer.multiplayer_peer = peer
+
+	if (!OS.has_feature("dedicated_server")):
+		$PlayerSpawner.spawn(self.get_multiplayer_authority())
+
+	print("hosting: %s" % { "address": address, "port": port, "tls": !!tls_options })
+
+
+func connect_server(address: String, port: int) -> void:
+	var get_tls = func (paths: Array) -> TLSOptions:
+		for p in paths:
+			if (ResourceLoader.exists(p)):
+				var tls = TLSOptions.client(load(p))
+				if (tls): return tls
+		return null
+
+	var tls_options = get_tls.call([
+		config.get_client_certificate_path(),
+		"res://certificate.crt",
+	])
+
+	var protocol = "wss" if (tls_options) else "ws"
+	address = address if address else config.get_client_server_address()
+	port = port if port else config.get_client_port()
+
+	var peer: WebSocketMultiplayerPeer = WebSocketMultiplayerPeer.new()
+	peer.create_client("%s://%s:%d" % [protocol, address, port], tls_options)
+	self.multiplayer.multiplayer_peer = peer
+
+	print("connecting: %s" % { "address": address, "port": port, "tls": !!tls_options })
