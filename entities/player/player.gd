2c1fade275a0b36c283ccb140949dfd864a271e1
Nathan Iszlaub <nathan.iszlaub@gmail.com>
2024-12-11 18:56:39 +0800

    Refactor player input to be stored as `Array` instead of `Dictionary` for less network traffic

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,17 +1,14 @@
 class_name Player extends CharacterBody2D
 
 
-const COYOTE_TIME = 0.05
-const INPUT_BUFFER_TIME = 0.1
-const MAX_SPEED = 256.0
+const COYOTE_TIME: float = 0.05
+const INPUT_BUFFER_TIME: float = 0.1
+const MAX_SPEED: float = 256.0
 
-var can_jump = self.COYOTE_TIME
+var can_jump: float = self.COYOTE_TIME
 
-var input: Dictionary = {
-	"left": 0.0,
-	"right": 0.0,
-	"jump": 0.0,
-}
+var input: Array = [ 0.0, 0.0, 0.0 ];
+enum INPUT { LEFT, RIGHT, JUMP }
 
 
 static func instantiate(id: int) -> Player:
@@ -32,7 +29,7 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	self.update_velocity(input.right - input.left, delta)
+	self.update_velocity(self.input[INPUT.RIGHT] - self.input[INPUT.LEFT], delta)
 	self.move_and_slide()
 
 
@@ -42,14 +39,14 @@ func _input(event: InputEvent) -> void:
 
 	if (event.is_action("player_left")):
 		var strength = event.get_action_strength("player_left")
-		self.input.left = strength if (strength > 0.2) else 0.0
+		self.input[INPUT.LEFT] = strength if (strength > 0.2) else 0.0
 
 	if (event.is_action("player_right")):
 		var strength = event.get_action_strength("player_right")
-		self.input.right = strength if (strength > 0.2) else 0.0
+		self.input[INPUT.RIGHT] = strength if (strength > 0.2) else 0.0
 
 	if (event.is_action_pressed("player_jump")):
-		self.input.jump = self.INPUT_BUFFER_TIME
+		self.input[INPUT.JUMP] = self.INPUT_BUFFER_TIME
 
 	self.sync_input.rpc(self.input)
 
@@ -61,7 +58,7 @@ func _kinematics_sync() -> void:
 
 
 @rpc("reliable")
-func sync_input(updated_input: Dictionary) -> void:
+func sync_input(updated_input: Array) -> void:
 	self.input = updated_input
 
 
@@ -76,13 +73,13 @@ func update_coyote_time(delta: float) -> void:
 		self.can_jump = self.COYOTE_TIME
 	else:
 		self.can_jump = max(0, self.can_jump - delta)
-		self.input.jump = max(0, self.input.jump - delta)
+		self.input[INPUT.JUMP] = max(0, self.input[INPUT.JUMP] - delta)
 
 
 func update_velocity(input_x: float, delta: float) -> void:
 	self.velocity.x = lerp(self.velocity.x, input_x * self.MAX_SPEED, delta * (1 + (int(self.is_on_floor()) * 3)))
-	if (self.input.jump && self.can_jump):
-		self.input.jump = 0
+	if (self.input[INPUT.JUMP] && self.can_jump):
+		self.input[INPUT.JUMP] = 0
 		self.can_jump = 0
 		self.velocity.y = -256
 	if (!self.is_on_floor()):
@@ -93,7 +90,7 @@ func update_animation() -> void:
 	var sprite = $AnimatedSprite2D as AnimatedSprite2D
 	sprite.flip_h = sprite.flip_h if (!self.velocity.x) else (self.velocity.x > 0)
 	if (self.is_on_floor()):
-		sprite.play("run" if (input.left || input.right) else "idle")
+		sprite.play("run" if (self.input[INPUT.LEFT] || self.input[INPUT.RIGHT]) else "idle")
 	else:
 		sprite.animation = "jump"
 		sprite.frame = 0 if (self.velocity.y < 0) else 1
